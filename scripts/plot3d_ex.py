c37b3cd0fb6f315d2aea6f6ac0e727c571b28cb6
FTryTE <148714110+FTryTE@users.noreply.github.com>
2023-10-22 20:54:57 +0300

    Update plot3d_ex.py
    SlavaK

--- scripts/plot3d_ex.py
+++ scripts/plot3d_ex.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 fig = plt.figure()
-ax = Axes3D(fig)
+ax = plt.axes(projection='3d')
 X = np.arange(-4, 4, 0.25)
 Y = np.arange(-4, 4, 0.25)
 X, Y = np.meshgrid(X, Y)
